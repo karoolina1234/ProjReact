bf4b2b9484ca6204379dce43f299ae21c71c67d1
karoolina1234 <karoolina.sm@gmail.com>
2020-12-17 00:56:28 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { Button, Form, Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logoSite from './logo.png';
+import { Component, useEffect } from 'react';
+import api from './api'
+import React, {useState} from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [dados, setDados] = useState([]);
+  const [numero, setNumeros] = useState([]);
+
+  
+  const dadosArr = Object.values(dados);
+ 
+
+  console.log(dadosArr);
+
+  useEffect(()=>{
+
+    api.get(`http://localhost:3001/encomendas/${numero}`).then((response) => {
+      setDados(response.data);
+    })
+
+  }, [dados], [numero]);
+return(
+
+   <div className="homePage">
+    <Container>
+    <div className="imagem-logo">
+    <img src={logoSite}/>
     </div>
+    <div className="form-busca">
+      <Form>
+    <Form.Group className="col-md-6" controlId="consulta">
+      <Form.Label className="consulta">Consulte sua encomenda</Form.Label>
+      <div className="input-busca">
+      <Form.Control type="text" placeholder="Digite um numero do pedido" onChange={(e)=>setNumeros(e.target.value)} />
+      </div>
+      <Button variant="primary" type="submit" >
+      <FontAwesomeIcon icon={faSearch} />
+    </Button>
+    </Form.Group>
+    </Form></div>
+    <div className="dados-busca">
+      
+      {dadosArr.map(dado=>(
+        <div >
+        <div className="dados">
+          <h3>{dado.nome}</h3>
+          <h3>{dado.id}</h3>
+        </div>
+        <div className="dados">
+          <h3>{dado.data}</h3>
+          
+          <h3>{dado.entregue}</h3>
+          
+        </div>
+        </div>
+      ))}
+        
+    </div>
+  </Container>
+    </div>
+    
+  
   );
 }
 
